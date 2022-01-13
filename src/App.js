1d3e2255dcf6d334132b879be5e2d5bf2c52e45a
bluretrece <luisvegasmor@gmail.com>
2022-01-13 17:25:43 -0400

    Setup and models

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  softShadows,
+  PresentationControls,
+  useHelper,
+  Environment,
+  OrbitControls,
+} from "@react-three/drei";
+import "./App.scss";
+import Header from "./components/Header";
+import Phone from "./models/Phone";
+import { SpotLightHelper } from "three";
+import { useSpring, a } from "@react-spring/three";
+import { a as web } from "@react-spring/web";
+softShadows();
 
-function App() {
+const Lights = () => {
+  const light = useRef();
+  // useHelper(light, SpotLightHelper, "cyan");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <group>
+      <ambientLight />
+      <spotLight ref={light} position={[50, 40, 40]} castShadow />
+    </group>
   );
-}
+};
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const props = useSpring({ open: Number(open) });
+  return (
+    <>
+      <web.main
+        style={{ background: props.open.to([0, 1], ["#2d4967", "#d25578"]) }}
+      >
+        <Canvas
+          colorManagement
+          shadows
+          dpr={[1, 2]}
+          // frameloop="demand"
+          camera={{ fov: 35 }}
+        >
+          <Suspense fallback={null}>
+            <Lights />
+            <ambientLight intensity={0.5} />
+            <directionalLight
+              castShadow
+              position={[0, 10, 0]}
+              intensity={1.5}
+            />
+            <pointLight position={[10, 10, 10]} intensity={1.5} castShadow />
+            <mesh rotation-x={-Math.PI / 2} receiveShadow>
+              <planeBufferGeometry args={[100, 100]} attach="geometry" />
+              <shadowMaterial attach="material" opacity={0.5} />
+            </mesh>
+            <PresentationControls
+              global
+              config={{ mass: 2, tension: 500 }}
+              snap={{ mass: 4, tension: 1500 }}
+              rotation={[0, 0.3, 0]}
+              polar={[-Math.PI / 3, Math.PI / 3]}
+              azimuth={[-Math.PI / 1.4, Math.PI / 2]}
+            >
+              <group
+                rotation={[0, Math.PI, 0]}
+                onClick={(e) => (e.stopPropagation(), setOpen(!open))}
+              >
+                <Phone castShadow />
+              </group>
+            </PresentationControls>
+            <Environment preset="city" />
+            <OrbitControls enablePan={false} />
+            <gridHelper />
+          </Suspense>
+        </Canvas>
+      </web.main>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>Testing maldita sea</span>
+      </div>
+      <div className="episode">
+        <span>SiteOne test</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
