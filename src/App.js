054d0c76ddd1423fa7041ffd3829cd9f27225758
bluretrece <luisvegasmor@gmail.com>
2022-01-14 15:43:09 -0400

    Debug dependencies and others

--- src/App.js
+++ src/App.js
@@ -1,80 +1,101 @@
-import React, { Suspense, useRef, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, createRef, useRef, useEffect } from "react";
+import * as THREE from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import {
-  softShadows,
-  PresentationControls,
-  useHelper,
-  Environment,
+  PerspectiveCamera,
+  // useHelper,
+  // Environment,
+  Stars,
   OrbitControls,
+  TransformControls,
 } from "@react-three/drei";
 import "./App.scss";
-import Header from "./components/Header";
 import Phone from "./models/Phone";
-import { SpotLightHelper } from "three";
-import { useSpring, a } from "@react-spring/three";
-import { a as web } from "@react-spring/web";
-softShadows();
+// import { SpotLightHelper } from "three";
+// import { useSpring, a } from "@react-spring/three";
+// import { a as web } from "@react-spring/web";
 
-const Lights = () => {
-  const light = useRef();
-  // useHelper(light, SpotLightHelper, "cyan");
+// const Lights = () => {
+//   const light = useRef();
+//   // useHelper(light, SpotLightHelper, "cyan");
+//   return (
+//     <group>
+//       <ambientLight />
+//       <spotLight ref={light} position={[50, 40, 40]} castShadow />
+//     </group>
+//   );
+// };
+function ScrollContainer({ scroll, children }) {
+  const { viewport } = useThree();
+  const group = useRef();
+  useFrame((state, delta) => {
+    group.current.position.y = THREE.MathUtils.damp(
+      group.current.position.y,
+      viewport.height * scroll.current,
+      4,
+      delta
+    );
+    // Or: group.current.position.lerp(vec.set(0, viewport.height * scroll.current, 0), 0.1)
+  });
+  return <group ref={group}>{children}</group>;
+}
+
+const Cube = () => {
   return (
-    <group>
-      <ambientLight />
-      <spotLight ref={light} position={[50, 40, 40]} castShadow />
-    </group>
+    <mesh position={[5, -15, 2]}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" color="pink" />
+    </mesh>
   );
 };
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const props = useSpring({ open: Number(open) });
+  const scrollRef = useRef();
+  const scroll = useRef(0);
   return (
     <>
-      <web.main
-        style={{ background: props.open.to([0, 1], ["#2d4967", "#d25578"]) }}
+      <Canvas
+        onCreated={(state) => state.events.connect(scrollRef.current)}
+        colorManagement
+        shadows
+        dpr={[1, 2]}
       >
-        <Canvas
-          colorManagement
-          shadows
-          dpr={[1, 2]}
-          // frameloop="demand"
-          camera={{ fov: 35 }}
-        >
-          <Suspense fallback={null}>
-            <Lights />
-            <ambientLight intensity={0.5} />
-            <directionalLight
-              castShadow
-              position={[0, 10, 0]}
-              intensity={1.5}
-            />
-            <pointLight position={[10, 10, 10]} intensity={1.5} castShadow />
-            <mesh rotation-x={-Math.PI / 2} receiveShadow>
-              <planeBufferGeometry args={[100, 100]} attach="geometry" />
-              <shadowMaterial attach="material" opacity={0.5} />
-            </mesh>
-            <PresentationControls
-              global
-              config={{ mass: 2, tension: 500 }}
-              snap={{ mass: 4, tension: 1500 }}
-              rotation={[0, 0.3, 0]}
-              polar={[-Math.PI / 3, Math.PI / 3]}
-              azimuth={[-Math.PI / 1.4, Math.PI / 2]}
-            >
-              <group
-                rotation={[0, Math.PI, 0]}
-                onClick={(e) => (e.stopPropagation(), setOpen(!open))}
-              >
-                <Phone castShadow />
-              </group>
-            </PresentationControls>
-            <Environment preset="city" />
-            <OrbitControls enablePan={false} />
-            <gridHelper />
-          </Suspense>
-        </Canvas>
-      </web.main>
+        <fog attach="fog" args={["#272730", 16, 30]} />
+        <ambientLight intensity={0.75} />
+        <PerspectiveCamera makeDefault position={[0, 0, 9]} fov={75}>
+          <pointLight intensity={1} position={[-10, -25, -10]} />
+          <spotLight
+            castShadow
+            intensity={2.25}
+            angle={0.2}
+            penumbra={1}
+            position={[-25, 20, -15]}
+            shadow-mapSize={[1024, 1024]}
+            shadow-bias={-0.0001}
+          />
+        </PerspectiveCamera>
+        <Suspense fallback={null}>
+          <ScrollContainer scroll={scroll}>
+            <Phone castShadow />
+            <Cube />
+          </ScrollContainer>
+        </Suspense>
+        <OrbitControls enablePan={false} enableZoom={true} />
+        <Stars radius={400} depth={50} count={700} factor={10} />
+        <TransformControls />
+      </Canvas>
+      <div
+        ref={scrollRef}
+        onScroll={(e) =>
+          (scroll.current =
+            e.target.scrollTop /
+            (e.target.scrollHeight - e.target.clientHeight))
+        }
+        className="scroll"
+      >
+        <div style={{ height: `200vh`, pointerEvents: "none" }}></div>
+      </div>
+      <div className="layer" />
     </>
   );
 };
