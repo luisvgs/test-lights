fcd0fe4d38f22e9345dda312474147ee10e93ce3
Luis Vegas <luisvegasmor@gmail.com>
2022-02-09 12:06:21 -0400

    Nuevo componente de texto, interactua con el scrolling.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   OrbitControls,
   Environment,
   ScrollControls,
+  Text,
+  Loader,
   softShadows,
   Scroll,
 } from "@react-three/drei";
@@ -12,7 +14,6 @@ import "./App.scss";
 import UnoComponent from "./components/UnoModel";
 import SuComponent from "./components/SiteOne";
 import SideBar from "./components/SideBar";
-// import Fireflies from "./components/Fireflies";
 import Lights from "./components/Lights";
 import Rig from "./rig";
 import Plane from "./components/Plane";
@@ -24,42 +25,57 @@ import { EffectComposer, SSAO, Bloom } from "@react-three/postprocessing";
 const App = () => {
   return (
     <>
-      <Suspense fallback={<p>Loading..</p>}>
-        <div className="text-background">
-          <h1 class>
-            <p>Lorem ipsum dolor sit amet</p>
-          </h1>
-        </div>
-        <SideBar />
-        <Canvas
-          dpr={[1, 1.5]}
-          shadows
-          gl={{
-            physicallyCorrectLights: true,
-          }}
-        >
-          <fog attach="fog" args={["red", 50, 60]} />
-          <color attach="background" args={["#17171b"]} />
+      <SideBar />
+      <Canvas
+        dpr={[1, 1.5]}
+        shadows
+        gl={{
+          physicallyCorrectLights: true,
+        }}
+      >
+        <fog attach="fog" args={["red", 50, 60]} />
+        <color attach="background" args={["#17171b"]} />
 
-          <ambientLight color={"purple"} intensity={3.7} />
-          <Lights />
-          <SupportLights />
-          <Suspense fallback={null}>
-            <PerspectiveCamera fov={3} position={[0.111, -0.932, 2.251]}>
-              <ScrollControls damping={1} pages={2} horizontal={true}>
-                <Scroll>
-                  <UnoComponent />
-                  <Plane />
-                </Scroll>
-              </ScrollControls>
-            </PerspectiveCamera>
-            <Environment preset="city" />
-          </Suspense>
-          <Rig />
-          {/* <OrbitControls enableZoom={false} /> */}
-        </Canvas>
-        <div className="layer" />
-      </Suspense>
+        <ambientLight color={"purple"} intensity={3.7} />
+        <Lights />
+        <SupportLights />
+        <Suspense fallback={null}>
+          <PerspectiveCamera
+            fov={45}
+            position={[0.111, -0.932, 2.191]}
+            rotation={[0.0, -6.2, 0.0]}
+          >
+            <ScrollControls
+              infinite={false}
+              damping={1}
+              pages={2}
+              horizontal={true}
+            >
+              <Scroll>
+                <Text
+                  position={[-0.471, 1.2, 2]}
+                  scale={[1, 1, 0]}
+                  fontSize={0.080}
+                  color="white"
+    maxWidth={0.69}
+                  anchorX="center"
+                  anchorY="middle"
+                >
+                  Lorem ipsum dolor sit amet
+                </Text>
+                <UnoComponent />
+                <SuComponent />
+                <Plane />
+              </Scroll>
+            </ScrollControls>
+          </PerspectiveCamera>
+          <Environment preset="city" />
+        </Suspense>
+        <Rig />
+        {/* <OrbitControls enableZoom={false} /> */}
+      </Canvas>
+      <div className="layer" />
+      <Loader />
     </>
   );
 };
